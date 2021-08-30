6fc85f3bdac3522537457c22e8834aa9525b8e81
bradley0130 <stonebrad51@gmail.com>
2021-08-30 04:55:26 -0500

    This is all the code
    anywhere that says 'TOKEN' that is where the discord API token will go

---
+++ bot.py
@@ -0,0 +1,37 @@
+import discord
+import os
+import requests
+import json
+
+client = discord.Client()
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+  @client.event
+  async def on_message(message):
+    if message.author == client.user:
+      return
+
+    if message.content.startswith('.inspire'):
+      quote = get_quote()
+      await message.channel.send(quote)
+
+    if message.content.startswith('.help'):
+      await message.channel.send('Commands **.help** **.test** **.inspire** and **.info**')
+
+    if message.contest.startswith('.info'):
+      await message.channel.send('Im made in Python and you can find all my code here')
+    if message.content.startswith('.test'):
+      await message.channel.send('Fuck off, bitch!')
+
+my_secret = os.environ['TOKEN']
+
+client.run(os.getenv('TOKEN'))
