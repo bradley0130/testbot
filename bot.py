1ecb47a7c5e700d8384417c075cd061702ccda5c
bradley0130 <stonebrad51@gmail.com>
2021-08-30 05:26:58 -0500

    Update bot.py
    Added some new commands 
    .update
    now having it search for bad words
    
    Updated
    .help with he new command and other info

--- bot.py
+++ bot.py
@@ -2,8 +2,18 @@ import discord
 import os
 import requests
 import json
+import random
 
 client = discord.Client()
+
+bad_words = ["fuck", "shit"]
+
+stop = [
+  "Don't say that!",
+  "You should die now!",
+  "KYS bitch",
+  "Die in the shitter and kys in #general chat"
+]
 
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
@@ -20,17 +30,26 @@ async def on_ready():
     if message.author == client.user:
       return
 
-    if message.content.startswith('.inspire'):
+    msg = message.content
+
+    if msg.startswith('.inspire'):
       quote = get_quote()
       await message.channel.send(quote)
 
-    if message.content.startswith('.help'):
-      await message.channel.send('Commands **.help** **.test** **.inspire** and **.info**')
+    if msg.startswith('.help'):
+      await message.channel.send('Commands **.help** **.test** **.inspire** **.update** if you say fuck or shit you get a cool message and **.info**')
 
-    if message.contest.startswith('.info'):
+    if msg.startswith('.info'):
       await message.channel.send('Im made in Python and you can find all my code here: https://github.com/bradley0130/testbot')
-    if message.content.startswith('.test'):
+      
+    if msg.startswith('.test'):
       await message.channel.send('Fuck off, bitch!')
+    
+    if msg.startswith('.update'):
+      await message.channel.send('I no longer have a use bradley will stop updating me on replit and find another way to make discord bots. Until then this is the last update for this bot')
+    
+    if any(word in msg for word in bad_words):
+      await message.channel.send(random.choice(stop))
 
 my_secret = os.environ['TOKEN']
 
